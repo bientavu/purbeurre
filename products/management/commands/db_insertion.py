ac6deaad472b504a8e7d0d47a40a832e5504e823
Axel <axel.mesnard@kedgebs.com>
2021-06-26 11:08:29 +0200

    full django reorg

--- products/management/commands/db_insertion.py
+++ products/management/commands/db_insertion.py
@@ -1,11 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
-from purbeurre.products.models import Product
-from purbeurre.products.openfoodfacts import ProductCleaner, ProductDownloader
+from products.models import Product
+from products.openfoodfacts import ProductCleaner, ProductDownloader
 
 class Command(BaseCommand):
     help = 'Add the products to the database'
 
-    def handle(self, ):
+    def handle(self, *args, **options):
         products = ProductDownloader()
         cleaner = ProductCleaner()
         products_dict = products.get_products_info()
@@ -16,11 +16,11 @@ class Command(BaseCommand):
                 name=product['product_name'],
                 url=product['url'],
                 image_url=product['image_url'],
-                nutriscore=product['nutriscore'],
+                nutriscore=product['nutriscore_grade'],
                 fat_100g=product['fat_100g'],
                 satured_fat_100g=product['saturated-fat_100g'],
                 salt_100g=product['salt_100g'],
-                sugar_100g=product['sugar_100g']
+                sugar_100g=product['sugars_100g']
                 )
             
             insertion.save()
