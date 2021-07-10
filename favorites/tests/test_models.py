03fa05ea238b9227b99430999882a55390b96b0e
Axel <axel.mesnard@kedgebs.com>
2021-07-10 11:17:20 +0200

    favorites test update

--- favorites/tests/test_models.py
+++ favorites/tests/test_models.py
@@ -11,15 +11,16 @@ class FavoriteModelTest(TestCase):
                                                    fat_100g=0.2,
                                                    satured_fat_100g=0.5, salt_100g=0.1, sugar_100g=0.8)
         product_insertion.categories.add(Category.objects.create(name='Pates à tartiner'))
-        product_insertion = Product.objects.create(name='Compote de pomme', url='http://helloworld2.com',
-                                                   image_url='http://helloworld.com/image2', nutriscore='a',
-                                                   fat_100g=0.1,
-                                                   satured_fat_100g=0.4, salt_100g=0.1, sugar_100g=0.3)
-        product_insertion.categories.add(Category.objects.create(name='Compote'))
-        Favorite.objects.create(product_to_substitute=Product.objects.get(id=1),
-                                product_to_substitute_id=Product.objects.get(id=2))
+        product_insertion_2 = Product.objects.create(name='Compote de pomme', url='http://helloworld2.com',
+                                                     image_url='http://helloworld.com/image2', nutriscore='a',
+                                                     fat_100g=0.1,
+                                                     satured_fat_100g=0.4, salt_100g=0.1, sugar_100g=0.3)
+        product_insertion_2.categories.add(Category.objects.create(name='Compote'))
+
+        Favorite.objects.create(product_to_substitute=Product.objects.get(id=1).id,
+                                product_to_substitute_id=Product.objects.get(id=2).id)
 
     def test_product_to_substitute_related_name(self):
         favorite = Favorite.objects.get(id=1)
         related_name = favorite._meta.get_field('product_to_substitute').related_name
-        self.assertEquals(related_name, 'products')
+        self.assertEquals(related_name, 'favorites_as_product')
