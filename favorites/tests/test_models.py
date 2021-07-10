69635ccb1bb26c2f1967b80bf2ad40bc4b5eb1ce
Axel <axel.mesnard@kedgebs.com>
2021-07-10 10:59:24 +0200

    working on tests

--- favorites/tests/test_models.py
+++ favorites/tests/test_models.py
@@ -1,1 +1,25 @@
- 
+from django.test import TestCase
+from products.models import Category, Product
+from favorites.models import Favorite
+
+
+class FavoriteModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        product_insertion = Product.objects.create(name='Nutella', url='http://helloworld.com',
+                                                   image_url='http://helloworld.com/image', nutriscore='b',
+                                                   fat_100g=0.2,
+                                                   satured_fat_100g=0.5, salt_100g=0.1, sugar_100g=0.8)
+        product_insertion.categories.add(Category.objects.create(name='Pates à tartiner'))
+        product_insertion = Product.objects.create(name='Compote de pomme', url='http://helloworld2.com',
+                                                   image_url='http://helloworld.com/image2', nutriscore='a',
+                                                   fat_100g=0.1,
+                                                   satured_fat_100g=0.4, salt_100g=0.1, sugar_100g=0.3)
+        product_insertion.categories.add(Category.objects.create(name='Compote'))
+        Favorite.objects.create(product_to_substitute=Product.objects.get(id=1),
+                                product_to_substitute_id=Product.objects.get(id=2))
+
+    def test_product_to_substitute_related_name(self):
+        favorite = Favorite.objects.get(id=1)
+        related_name = favorite._meta.get_field('product_to_substitute').related_name
+        self.assertEquals(related_name, 'products')
--- products/tests/test_models.py
+++ products/tests/test_models.py
@@ -9,3 +9,45 @@ class CategoryModelTest(TestCase):
 
     def test_name_label(self):
         category = Category.objects.get(id=1)
+        field_label = category._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'category_name')
+
+    def test_name_max_length(self):
+        category = Category.objects.get(id=1)
+        field_label = category._meta.get_field('name').max_length
+        self.assertEquals(field_label, 100)
+
+
+class ProductModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        product_insertion = Product.objects.create(name='Nutella', url='http://helloworld.com',
+                                                   image_url='http://helloworld.com/image', nutriscore='b',
+                                                   fat_100g=0.2,
+                                                   satured_fat_100g=0.5, salt_100g=0.1, sugar_100g=0.8)
+        product_insertion.categories.add(Category.objects.create(name='Pates à tartiner'))
+
+    def test_name_max_length(self):
+        product = Product.objects.get(id=1)
+        max_length = product._meta.get_field('name').max_length
+        self.assertEquals(max_length, 200)
+
+    def test_categories_help_text_name(self):
+        product = Product.objects.get(id=1)
+        help_text = product._meta.get_field('categories').help_text
+        self.assertEquals(help_text, 'Select a category for this product')
+
+    # def test_categories_related_name(self):
+    #     product = Product.objects.get(id=1)
+    #     related_name = product._meta.get_field('categories').related_name
+    #     self.assertEquals(related_name, 'products')
+
+    def test_url_max_length(self):
+        product = Product.objects.get(id=1)
+        max_length = product._meta.get_field('url').max_length
+        self.assertEquals(max_length, 300)
+
+    def test_image_url_blank_is_true(self):
+        product = Product.objects.get(id=1)
+        blank_is_true = product._meta.get_field('image_url').blank
+        self.assertEquals(blank_is_true, True)
