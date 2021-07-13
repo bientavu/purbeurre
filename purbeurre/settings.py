e50bdd5d13d8ed7a69da8d92b242ac84c9e2f395
Axel <axel.mesnard@kedgebs.com>
2021-07-13 15:40:21 +0200

    small typo

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +41,8 @@ INSTALLED_APPS = [
     'products.apps.ProductsAndCategoriesConfig',
     'favorites.apps.FavoritesConfig',
     'search.apps.SearchConfig',
-    'accounts'
+    'accounts',
+    'pages'
 ]
 
 MIDDLEWARE = [
@@ -126,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
