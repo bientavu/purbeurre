95818b984af233eb948fcee5db342d69802f3b72
Axel <axel.mesnard@kedgebs.com>
2021-07-24 16:14:22 +0200

    urls update

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from products import views
 
 
 urlpatterns = [
-    path('results/', views.products_list, name='products_list')
+    path('results/', views.products_list, name='products_list'),
+    path('results/<int:product_id>/', views.product_detail, name='product_detail')
 ]
