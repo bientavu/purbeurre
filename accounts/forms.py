3a041ae30da12054382b57433a4d000387dc7841
Axel <axel.mesnard@kedgebs.com>
2021-07-24 11:44:36 +0200

    form updated

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,9 +11,21 @@ class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['first_name'].label = 'Prénom'
+        self.fields['last_name'].label = 'Nom'
+        self.fields['birth_date'].label = 'Date de naissance'
+        self.fields['password1'].label = 'Mot de passe'
+        self.fields['password2'].label = 'Confirmation mot de passe'
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('first_name', 'last_name', 'email', 'birth_date')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].label = 'Prénom'
+        self.fields['last_name'].label = 'Nom'
+        self.fields['birth_date'].label = 'Date de naissance'
+        self.fields['password1'].label = 'Mot de passe'
+        self.fields['password2'].label = 'Confirmation mot de passe'
