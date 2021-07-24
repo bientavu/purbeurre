c08f50a5ef5f847755699d3e31a7a26723883b43
Axel <axel.mesnard@kedgebs.com>
2021-07-24 16:14:54 +0200

    added views

--- products/views.py
+++ products/views.py
@@ -1,8 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from products.models import Product, Category
 
 
 def products_list(request):
-    products = Product.objects.get(id=1)
+    products = Product.objects.all()[:6]
     return render(request, 'results.html', {'products': products})
 
+
+def categories_list(request):
+    categories = Category.objects.all()
+    return render(request, 'results.html', {'categories': categories})
+
+
+def product_detail(request, product_id):
+    products = Product.objects.all()[:6]
+    return render(request, 'results.html', {'products': products})
