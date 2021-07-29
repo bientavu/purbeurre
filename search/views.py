bf80bb0a279e955c2b2977342c899e3f47f5705d
Axel <axel.mesnard@kedgebs.com>
2021-07-29 11:42:55 +0200

    working on search bar

--- search/views.py
+++ search/views.py
@@ -1,3 +1,14 @@
+from django.db.models import Q
 from django.shortcuts import render
+from django.views.generic import ListView
+from products.models import Product
 
-# Create your views here.
+
+class SearchResultsView(ListView):
+    model = Product
+    template_name = 'products/results.html'
+
+    def get_queryset(self):
+        return Product.objects.filter(
+            Q(name__icontains='Nutella')
+        )
