515fc5d9c4fa75e9ff924a214bb5f1da96957515
Axel <axel.mesnard@kedgebs.com>
2021-08-04 10:31:54 +0200

    product views cleaned

--- products/views.py
+++ products/views.py
@@ -3,16 +3,6 @@ from django.shortcuts import render
 from products.models import Product, Category
 
 
-def products_list(request):
-    products = Product.objects.all()[:6]
-    return render(request, 'products/results.html', {'products': products})
-
-
-def categories_list(request):
-    categories = Category.objects.all()
-    return render(request, 'products/results.html', {'categories': categories})
-
-
 def product_detail(request, product_id):
     product = Product.objects.get(id=product_id)
     return render(request, 'products/details.html', {'product': product})
