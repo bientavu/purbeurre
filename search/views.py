d60857fbffbf019f2ca247aed7bbd16a755f6334
Axel <axel.mesnard@kedgebs.com>
2021-08-04 10:32:18 +0200

    search view cleaned + new search bar errors messages

--- search/views.py
+++ search/views.py
@@ -1,40 +1,27 @@
-from django.db.models import Q
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from products.models import Product, Category
 
 
-# class SearchResultsView(ListView):
-#     model = Product
-#     template_name = 'products/results.html'
-#
-#     def get_queryset(self):
-#         query = self.request.GET.get('q')
-#         searched_product = Product.objects.filter(Q(name__icontains=query))
-#         return searched_product
-
-
-# def search_results(request):
-#     product_name = request.GET.get('q')
-#     searched_product = Product.objects.filter(Q(name__icontains=product_name))
-#     return render(request, 'products/results.html', {'searched_product': searched_product})
-
 def search_results(request):
-    if request.method == 'POST':
-        searched = request.POST['searched']
+    # if request.method == 'POST':
+    searched = request.POST['searched']
+    if not searched or searched == "":
+        messages.error(request, "Veuillez écrire un produit")
+        return redirect('home')
+    else:
         searched_product = Product.objects.filter(
             name__icontains=searched,
             nutriscore__in=['c', 'd', 'e']
         ).first()
-        substitutes_products = Product.objects.filter(
-            name__icontains=searched,
-            nutriscore__in=['a', 'b'],
-            categories__name__contains=searched_product.categories.first()
-        )
-        context = {'searched_product': searched_product, 'substitutes_products': substitutes_products}
-        return render(request, 'products/results.html', context)
-
-
-def categories_list(request):
-    categories = Category.objects.all()
-    return render(request, 'products/results.html', {'categories': categories})
+        if searched_product is None:
+            messages.error(request, "Désolé nous n'avons pas trouvé ce produit")
+            return redirect('home')
+        else:
+            substitutes_products = Product.objects.filter(
+                name__icontains=searched,
+                nutriscore__in=['a', 'b'],
+                categories__name__contains=searched_product.categories.first()
+            )
+            context = {'searched_product': searched_product, 'substitutes_products': substitutes_products}
+            return render(request, 'products/results.html', context)
