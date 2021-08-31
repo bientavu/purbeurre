8c4c038403e7eaf9d041dc4ef3750e034298527f
Axel <axel.mesnard@kedgebs.com>
2021-08-31 15:45:23 +0200

    pass protection

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'purbeurre',
         'USER': 'postgres',
-        'PASSWORD': 'azqsazqs282',
+        'PASSWORD': os.getenv('db_password'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
