242923c7b3f66a2e27e036e55e20b45a139148ff
Axel <axel.mesnard@kedgebs.com>
2021-09-07 18:02:40 +0200

    test view corrected

--- search/tests/test_views.py
+++ search/tests/test_views.py
@@ -11,7 +11,7 @@ def test_search_view(auto_login_user):
         name='Test products',
         url='testurl.test',
         image_url='test.testimage.com',
-        nutriscore='c',
+        nutriscore='d',
         fat_100g='0.2',
         satured_fat_100g='0.85',
         sugar_100g='0.4',
@@ -21,7 +21,7 @@ def test_search_view(auto_login_user):
         name='Test_products_2',
         url='testurl2.test2',
         image_url='test2.testimage2.com',
-        nutriscore='a',
+        nutriscore='d',
         fat_100g='0.4',
         satured_fat_100g='0.9',
         sugar_100g='1.4',
@@ -33,10 +33,11 @@ def test_search_view(auto_login_user):
         substitute_product_id='2',
         user_id='1'
     )
+    data = {'searched': 'products_2'}
     url = reverse('search_results')
-    response = client.get(url)
-    assert response.status_code == 302
+    response = client.post(url, data)
+    assert response.status_code == 200
     assert product_2.name in str(response.content)
     assert favorites.product_to_substitute_id in str(response.content)
-    assert product_2.nutriscore.encode() in response.content
-    assert product_2.sugar_100g.encode() in response.content
+    assert product_2.name in str(response.content)
+    assert product_2.image_url in str(response.content)
