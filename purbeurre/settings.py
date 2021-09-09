9388724177a5f01ff584d1d9332fe67b0755a0a9
Axel <axel.mesnard@kedgebs.com>
2021-09-09 16:58:41 +0200

    heroku update

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -143,7 +143,7 @@ LOGGING = {
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'purbeurre/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
