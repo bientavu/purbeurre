1ca08851ac4f347d61237342c17f09aa02112fde
Axel <axel.mesnard@kedgebs.com>
2021-09-09 12:15:06 +0200

    selenium test working

--- accounts/tests/test_functional.py
+++ accounts/tests/test_functional.py
@@ -1,16 +1,27 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-# Create your tests here.
 class UserFormTest(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome(ChromeDriverManager().install())
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
 
     def testform(self):
         selenium = webdriver.Chrome()
-        # Choose your url to visit
+
         selenium.get('http://127.0.0.1:8000/accounts/signup/')
-        # find the elements you need to submit form
+
         first_name = selenium.find_element_by_id('id_first_name')
         last_name = selenium.find_element_by_id('id_last_name')
         email = selenium.find_element_by_id('id_email')
@@ -18,9 +29,8 @@ class UserFormTest(LiveServerTestCase):
         id_password1 = selenium.find_element_by_id('id_password1')
         id_password2 = selenium.find_element_by_id('id_password2')
 
-        submit = selenium.find_element_by_class_name('submit_button')
+        submit = selenium.find_element_by_id('submit_button')
 
-        # populate the form with data
         first_name.send_keys('Jean')
         last_name.send_keys('Bon')
         email.send_keys('test@test.com')
@@ -28,8 +38,7 @@ class UserFormTest(LiveServerTestCase):
         id_password1.send_keys('helloworld')
         id_password2.send_keys('helloworld')
 
-        # submit form
         submit.send_keys(Keys.RETURN)
 
-        # check result; page source looks at entire html document
         assert 'Jean' in selenium.page_source
+        assert 'test@test.com' in selenium.page_source
