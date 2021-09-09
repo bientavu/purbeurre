5a2c397c8523026cab6ad8e415fffe471ec7d587
Axel <axel.mesnard@kedgebs.com>
2021-09-09 16:34:05 +0200

    heroku update

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'purbeurre/static/dist'),
+    os.path.join(BASE_DIR, 'purbeurre/static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
