1aac1ab363d32818938044b1b212fd49047de502
Axel <axel.mesnard@kedgebs.com>
2021-09-16 09:28:59 +0200

    minor change

--- products/openfoodfacts.py
+++ products/openfoodfacts.py
@@ -8,7 +8,6 @@ class ProductDownloader:
         Gets all the products information from the
         openfoodfacts API and put them in a list
         """
-        global response
         url = "https://fr.openfoodfacts.org/cgi/search.pl"
         number_of_pages = 8
         page_size = 1000
@@ -30,7 +29,7 @@ class ProductDownloader:
             response = requests.get(url, params=params)
             products.extend(response.json()["products"])
 
-        return products, response
+        return products
 
 
 class ProductCleaner:
