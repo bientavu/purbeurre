ea5e4b2aa54d11e03bdad33b713dc2ebcf21e57d
Axel <axel.mesnard@kedgebs.com>
2021-10-06 13:43:38 +0200

    add travis and production settings

---
+++ purbeurre/settings/travis.py
@@ -0,0 +1,12 @@
+from purbeurre.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
