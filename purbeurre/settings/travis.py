49315d227194105ba090de99c202f8cbde87c7a2
Axel <axel.mesnard@kedgebs.com>
2021-10-07 14:37:45 +0200

    travis correction async 3

--- purbeurre/settings/travis.py
+++ purbeurre/settings/travis.py
@@ -1,6 +1,9 @@
 from purbeurre.settings import *
 import os
+from django.core.wsgi import get_wsgi_application
 
+application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purbeurre.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 DATABASES = {
