92a94dc373584c033ed4908349e776d1ba80dc8f
Axel <axel.mesnard@kedgebs.com>
2021-10-07 18:18:52 +0200

    travis test 2

--- conftest.py
+++ conftest.py
@@ -1,23 +1,23 @@
-# import pytest
-# from django.test import Client as DjangoClient
-# from accounts.models import CustomUser
-#
-#
-# @pytest.fixture
-# def user(db):
-#     """Create a user"""
-#     user = CustomUser.objects.create_superuser(
-#         birth_date='1992-05-15',
-#         email='helloworld@hello.world',
-#         username='axel',
-#         password='test'
-#     )
-#     return user
-#
-#
-# @pytest.fixture
-# def connected_client(user):
-#     """Log in the user"""
-#     connected_client = DjangoClient()
-#     connected_client.force_login(user)
-#     return connected_client
+import pytest
+from django.test import Client as DjangoClient
+from accounts.models import CustomUser
+
+
+@pytest.fixture
+def user(db):
+    """Create a user"""
+    user = CustomUser.objects.create_superuser(
+        birth_date='1992-05-15',
+        email='helloworld@hello.world',
+        username='axel',
+        password='test'
+    )
+    return user
+
+
+@pytest.fixture
+def connected_client(user):
+    """Log in the user"""
+    connected_client = DjangoClient()
+    connected_client.force_login(user)
+    return connected_client
