dc28b130f13e819d366da641234ed1b58c40258b
Axel <axel.mesnard@kedgebs.com>
2021-10-27 09:26:19 +0200

    search view test has been refactored

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from django.test import Client as DjangoClient
 from accounts.models import CustomUser
+from products.models import Product
 
 
 @pytest.fixture
@@ -21,3 +22,28 @@ def connected_client(user):
     connected_client = DjangoClient()
     connected_client.force_login(user)
     return connected_client
+
+
+@pytest.fixture
+def products_creation():
+    product_1, _ = Product.objects.get_or_create(
+        name='Test products',
+        url='testurl.test',
+        image_url='test.testimage.com',
+        nutriscore='d',
+        fat_100g='0.2',
+        satured_fat_100g='0.85',
+        sugar_100g='0.4',
+        salt_100g='1.2'
+    )
+    product_2, _ = Product.objects.get_or_create(
+        name='Test_products_2',
+        url='testurl2.test2',
+        image_url='test2.testimage2.com',
+        nutriscore='d',
+        fat_100g='0.4',
+        satured_fat_100g='0.9',
+        sugar_100g='1.4',
+        salt_100g='1.5'
+    )
+    return product_1, product_2
--- search/tests/test_views.py
+++ search/tests/test_views.py
@@ -6,32 +6,13 @@ from favorites.models import Favorite
 
 
 @pytest.mark.django_db
-def test_search_view(connected_client, user):
+def test_search_view(connected_client, user, products_creation):
     """
     Test the search view by creating products and favorites
     Then we test the response.content to check if we have
     the informations we need (product name, status code, etc)
     """
-    product_1, _ = Product.objects.get_or_create(
-        name='Test products',
-        url='testurl.test',
-        image_url='test.testimage.com',
-        nutriscore='d',
-        fat_100g='0.2',
-        satured_fat_100g='0.85',
-        sugar_100g='0.4',
-        salt_100g='1.2'
-    )
-    product_2, _ = Product.objects.get_or_create(
-        name='Test_products_2',
-        url='testurl2.test2',
-        image_url='test2.testimage2.com',
-        nutriscore='d',
-        fat_100g='0.4',
-        satured_fat_100g='0.9',
-        sugar_100g='1.4',
-        salt_100g='1.5'
-    )
+    product_1, product_2 = products_creation
     favorites, _ = Favorite.objects.get_or_create(
         product_to_substitute_id=product_1.id,
         substitute_product_id=product_2.id,
